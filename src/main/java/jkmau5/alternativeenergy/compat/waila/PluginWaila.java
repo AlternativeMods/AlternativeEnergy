f7dc3ab959de692f7653a35f44f6b83e09bfc427
jk-5 <jeffreykog@me.com>
2013-12-07 15:03:56 +0100

    Add dummy waila plugin

---
+++ src/main/java/jkmau5/alternativeenergy/compat/waila/PluginWaila.java
@@ -0,0 +1,28 @@
+package jkmau5.alternativeenergy.compat.waila;
+
+import jkmau5.alternativeenergy.compat.ICompatPlugin;
+
+/**
+ * No description given
+ *
+ * @author jk-5
+ */
+public class PluginWaila implements ICompatPlugin {
+
+    @Override
+    public String getModID() {
+        return "waila";
+    }
+
+    @Override
+    public void preInit() {
+    }
+
+    @Override
+    public void init() {
+    }
+
+    @Override
+    public void postInit() {
+    }
+}
