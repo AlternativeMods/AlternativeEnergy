02c886547c3fac28bbd22bbeea1348c090d2d5b8
jk-5 <jeffreykog@me.com>
2013-12-07 22:54:47 +0100

    Add some unit tests. Lots more to come tomorrow

---
+++ src/test/java/jkmau5/alternativeenergy/AltEngSupportTest.java
@@ -0,0 +1,36 @@
+package jkmau5.alternativeenergy;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * No description given
+ *
+ * @author jk-5
+ */
+public class AltEngSupportTest {
+
+    @Test
+    public void testInitiateNbtTag(){
+        ItemStack stack = new ItemStack(0, 1, 0);
+        Assert.assertNull(stack.getTagCompound());
+        AltEngSupport.initiateNBTTag(stack);
+        Assert.assertNotNull(stack.getTagCompound());
+    }
+
+    @Test
+    public void testCanWrench(){
+        ItemStack wrench = new ItemStack(0, 1, 0);
+        NBTTagCompound tag = new NBTTagCompound();
+        tag.setInteger("storedPower", 50);
+        wrench.setTagCompound(tag);
+
+        Assert.assertFalse(AltEngSupport.canWrench(wrench));
+        tag.setInteger("storedPower", 250);
+        Assert.assertTrue(AltEngSupport.canWrench(wrench));
+        tag.setInteger("storedPower", 300);
+        Assert.assertTrue(AltEngSupport.canWrench(wrench));
+    }
+}
