6b7fd48fedc1a9900dc35c3c5d4ae00320ad933e
jk-5 <jeffreykog@me.com>
2014-04-02 21:14:27 +0200

    Shut up. Stupid exception

--- src/main/scala/alternativemods/alteng/tileentities/TileEntityConveyor.scala
+++ src/main/scala/alternativemods/alteng/tileentities/TileEntityConveyor.scala
@@ -2,9 +2,6 @@ package alternativemods.alteng.tileentities
 
 import net.minecraft.tileentity.TileEntity
 import net.minecraft.nbt.NBTTagCompound
-import net.minecraftforge.common.util.ForgeDirection
-import net.minecraft.network.NetworkManager
-import net.minecraft.network.play.server.S35PacketUpdateTileEntity
 import alternativemods.alteng.network.SyncedTileEntity
 import io.netty.buffer.ByteBuf
 
@@ -14,6 +11,7 @@ import io.netty.buffer.ByteBuf
  * Time: 17:00
  */
 class TileEntityConveyor(var facing: Int = 0) extends TileEntity with SyncedTileEntity {
+  def this(){this(0)}
 
   override def readFromNBT(tag: NBTTagCompound) = {
     super.readFromNBT(tag)
