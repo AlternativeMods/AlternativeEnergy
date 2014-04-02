cf399a190a7ce3c731183181ffbc0257b6e44f5f
Lordmau5 <dustin.hecken@web.de>
2014-04-02 19:38:32 +0200

    <changes>

--- src/main/scala/alternativemods/alteng/blocks/BlockConveyor.scala
+++ src/main/scala/alternativemods/alteng/blocks/BlockConveyor.scala
@@ -4,10 +4,12 @@ import net.minecraft.block.Block
 import net.minecraft.block.material.Material
 import net.minecraft.world.{IBlockAccess, World}
 import alternativemods.alteng.tileentities.TileEntityConveyor
-import net.minecraft.util.IIcon
+import net.minecraft.util.{MathHelper, IIcon}
 import net.minecraft.client.renderer.texture.IIconRegister
 import net.minecraftforge.common.util.ForgeDirection
 import net.minecraft.creativetab.CreativeTabs
+import net.minecraft.entity.EntityLivingBase
+import net.minecraft.item.ItemStack
 
 /**
  * Author: Lordmau5
@@ -18,21 +20,29 @@ class BlockConveyor(material: Material) extends Block(material) {
 
   setCreativeTab(CreativeTabs.tabBlock)
   setBlockBounds(0.0F, 0.0F, 0.0F, 1.0F, 0.3F, 1.0F)
+
+  override def onBlockPlacedBy(world: World, x: Int, y: Int, z: Int, entity: EntityLivingBase, item: ItemStack) = {
+    val dir = MathHelper.floor_double(((entity.rotationYaw * 4F) / 360F) + 0.5D) & 3
+    world.setTileEntity(x, y, z, new TileEntityConveyor(dir))
+  }
+
+
   override def createTileEntity(world: World, metadata: Int) = new TileEntityConveyor()
 
   override def hasTileEntity(metadata: Int) = true
 
+  val facings = Array[ForgeDirection](ForgeDirection.NORTH, ForgeDirection.EAST, ForgeDirection.SOUTH, ForgeDirection.WEST)
   val top = Array.ofDim[IIcon](4)
   val icons = Array.ofDim[IIcon](2)
-  override def registerBlockIcons(iR : IIconRegister) = {
+  override def registerBlockIcons(iR: IIconRegister) = {
     for(meta <- 0 until top.length) {
-      top(meta) = iR.registerIcon("alternativeenergy:conveyor/top_" + ForgeDirection.getOrientation(meta + 2).name())
+      top(meta) = iR.registerIcon("alternativeenergy:conveyor/top_" + facings(meta).name())
     }
     icons(0) = iR.registerIcon("alternativeenergy:conveyor/bottom")
     icons(1) = iR.registerIcon("alternativeenergy:conveyor/sides")
   }
 
-  override def getIcon(world : IBlockAccess, x : Int, y : Int, z : Int, side : Int): IIcon = {
+  override def getIcon(world: IBlockAccess, x: Int, y: Int, z: Int, side: Int): IIcon = {
     val worldTile = world.getTileEntity(x, y, z)
     if(!worldTile.isInstanceOf[TileEntityConveyor])
       return icons(1)
--- src/main/scala/alternativemods/alteng/tileentities/TileEntityConveyor.scala
+++ src/main/scala/alternativemods/alteng/tileentities/TileEntityConveyor.scala
@@ -2,6 +2,9 @@ package alternativemods.alteng.tileentities
 
 import net.minecraft.tileentity.TileEntity
 import net.minecraft.nbt.NBTTagCompound
+import net.minecraftforge.common.util.ForgeDirection
+import net.minecraft.network.{Packet, NetworkManager}
+import net.minecraft.network.play.server.S35PacketUpdateTileEntity
 
 /**
  * Author: Lordmau5
@@ -11,13 +14,24 @@ import net.minecraft.nbt.NBTTagCompound
 class TileEntityConveyor extends TileEntity {
   var facing: Int = 0
 
-  override def readFromNBT(tag : NBTTagCompound) = {
-    super.readFromNBT(tag)
-    facing = tag.getInteger("facing");
+  def this(facing: Int) {
+    this()
+    this.facing = facing
   }
 
-  override def writeToNBT(tag : NBTTagCompound) = {
+  override def readFromNBT(tag: NBTTagCompound) = {
+    super.readFromNBT(tag)
+    facing = tag.getInteger("facing")
+  }
+
+  override def writeToNBT(tag: NBTTagCompound) = {
     super.writeToNBT(tag)
     tag.setInteger("facing", facing)
   }
+
+  override def getDescriptionPacket: Packet = {
+    new S35PacketUpdateTileEntity()
+  }
+
+  override def onDataPacket(net: NetworkManager, pkt: S35PacketUpdateTileEntity) = readFromNBT(pkt.func_148857_g())
 }
