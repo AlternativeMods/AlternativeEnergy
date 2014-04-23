419b99b3c2c4e8ea4bf4fc329bc5394e7750cd96
jk-5 <jeffreykog@me.com>
2014-04-23 16:21:42 +0200

    Hi trait awesomeness

--- src/main/scala/alternativemods/alteng/content/blocks/tier1/tile/TileFluidEnergyProducer.scala
+++ src/main/scala/alternativemods/alteng/content/blocks/tier1/tile/TileFluidEnergyProducer.scala
@@ -6,14 +6,20 @@ import alternativemods.alteng.fluid.tank.RestrictedTank
 import alternativemods.alteng.content.AltEngContent
 import net.minecraftforge.common.util.ForgeDirection
 import net.minecraftforge.fluids.Fluid
+import alternativemods.alteng.powertraits.tile.UniversalPowerTile
+import alternativemods.alteng.util.Ratios
 
 /**
  * No description given
  *
  * @author jk-5
  */
-class TileFluidEnergyProducer extends TileEntity with SingleTankTile {
-  override val tank = new RestrictedTank(AltEngContent.fluidLiquidEnergy, "energyTank", 16000, this)
+class TileFluidEnergyProducer extends TileEntity with SingleTankTile with UniversalPowerTile {
+  var energy = 0d
+  val maxEnergy = 1000d
+  val ic2Ratio = Ratios.MJ
+  val bcRatio = Ratios.EU
+  val tank = new RestrictedTank(AltEngContent.fluidLiquidEnergy, "energyTank", 16000, this)
 
   override def canFill(from: ForgeDirection, fluid: Fluid) = false
 }
