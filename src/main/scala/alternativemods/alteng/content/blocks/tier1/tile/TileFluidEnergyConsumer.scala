5409d3b3fa1e653bab5916e3ce6d0feac929ce00
Lordmau5 <dustin.hecken@web.de>
2014-04-23 22:18:58 +0200

    Prevent possible dupe?

--- src/main/scala/alternativemods/alteng/content/blocks/tier1/tile/TileFluidEnergyConsumer.scala
+++ src/main/scala/alternativemods/alteng/content/blocks/tier1/tile/TileFluidEnergyConsumer.scala
@@ -22,7 +22,7 @@ class TileFluidEnergyConsumer extends TileEntity with UniversalPowerEjector with
   val tank = new RestrictedTank(AltEngContent.fluidLiquidEnergy, "energyTank", 16000, this)
 
   override def fill(from: ForgeDirection, resource: FluidStack, doFill: Boolean): Int = {
-    val drain = super.fill(from, resource, doFill)
+    val drain = super.fill(from, resource, doFill = false) // Don't insert into the tank *yet* :3
     if(doFill){
       this.energy += resource.amount //TODO: check max space
     }
